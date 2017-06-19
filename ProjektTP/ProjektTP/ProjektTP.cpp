e8e57ee95d63803964a71df04a4b5efbf392de19
jakub1899 <jakub1899@wp.pl>
2017-06-19 21:20:28 +0200

    Update ProjektTP.cpp

--- ProjektTP/ProjektTP/ProjektTP.cpp
+++ ProjektTP/ProjektTP/ProjektTP.cpp
@@ -17,6 +17,51 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+void PaintBackground(HDC hdc)
+{
+	Graphics graphics(hdc);
+	Pen pen(Color(255, 0, 0, 255),10);
+	graphics.DrawLine(&pen, 0, 450, 1500, 450);
+}
+
+void MyOnPaint(HDC hdc)
+{
+	Graphics lowerarm(hdc);
+	Graphics upperarm(hdc);
+	Pen pen(Color(255, 0, 0, 0), 5);
+	lowerarm.DrawLine(&pen, xbl,ybl,xel,yel);
+	upperarm.DrawLine(&pen, xbu, ybu, xeu, yeu);
+}
+
+void PaintSquares(HDC hdc)
+{
+	Graphics square1(hdc);
+	Graphics square2(hdc);
+	Graphics square3(hdc);
+	Graphics square4(hdc);
+	Graphics square5(hdc);
+	Graphics square6(hdc);
+	Pen pen(Color(255, 255, 0, 0), 5);
+	square1.DrawRectangle(&pen, 800, 430, 20, 20);
+	square2.DrawRectangle(&pen, 830, 430, 20, 20);
+	square3.DrawRectangle(&pen, 860, 430, 20, 20);
+	square4.DrawRectangle(&pen, 890, 430, 20, 20);
+	square5.DrawRectangle(&pen, 920, 430, 20, 20);
+	square6.DrawRectangle(&pen, 950, 430, 20, 20);
+}
+
+void repaintWindow(HWND hWnd, HDC &hdc, PAINTSTRUCT &ps, RECT *drawArea)
+{
+	if (drawArea == NULL)
+		InvalidateRect(hWnd, NULL, TRUE); // repaint all
+	else
+		InvalidateRect(hWnd, drawArea, TRUE); //repaint drawArea
+	hdc = BeginPaint(hWnd, &ps);
+	MyOnPaint(hdc);
+	PaintSquares(hdc);
+	EndPaint(hWnd, &ps);
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -101,7 +146,107 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Lower part up"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   100, 460,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON1,
+	   hInstance,
+	   NULL);
 
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Lower part down"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   100, 560,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON2,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Upper part up"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   250, 460,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON3,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Upper part down"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   250, 560,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON4,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Speed+"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   450, 460,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON5,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Speed-"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   450, 560,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON6,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Record Motion"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   650, 460,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON7,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Auto"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   650, 560,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON8,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Catch Object"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   850, 460,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON9,
+	   hInstance,
+	   NULL);
+
+   g_hbutton = CreateWindow(TEXT("button"),
+	   TEXT("Release Object"),
+	   WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+	   850, 560,
+	   150, 100,
+	   hWnd,
+	   (HMENU)ID_BUTTON10,
+	   hInstance,
+	   NULL);	
+	
    if (!hWnd)
    {
       return FALSE;
